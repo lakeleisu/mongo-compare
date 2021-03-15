1d633a5eef18ec967fa82b7eb0556e4aa0fd4157
lakeleisu <33753116+lakeleisu@users.noreply.github.com>
2021-03-15 17:52:04 +0800

    Update mongocompare.go

--- mongocompare/mongocompare.go
+++ mongocompare/mongocompare.go
@@ -4,7 +4,6 @@
 // not use this file except in compliance with the License. You may obtain
 // a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-// Package mongodump creates BSON data from the contents of a MongoDB instance.
 package mongocompare
 
 import (
